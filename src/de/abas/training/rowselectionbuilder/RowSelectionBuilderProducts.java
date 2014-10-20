3fd03c7d722a78dfd9005387f0f278f267facfd1
Jasmin Scheffel <jasc@abas.de>
2014-10-20 13:36:25 +0200

    EventHandling classes added, code formatted

--- src/de/abas/training/rowselectionbuilder/RowSelectionBuilderProducts.java
+++ src/de/abas/training/rowselectionbuilder/RowSelectionBuilderProducts.java
@@ -10,55 +10,61 @@ import de.abas.erp.db.selection.SelectionBuilder;
 import de.abas.erp.db.util.QueryUtil;
 import de.abas.training.common.AbstractAjoAccess;
 
+/**
+ * This class shows advanced selections in the head and row of a data set.
+ * 
+ * @author abas Software AG
+ *
+ */
 public class RowSelectionBuilderProducts extends AbstractAjoAccess {
 
-        @Override
-        public void run(String[] args) {
-                RowSelectionBuilder<Product, Row> rowSelectionBuilder =
-                                RowSelectionBuilder.create(Product.class, Row.class);
+	@Override
+	public void run(String[] args) {
+		RowSelectionBuilder<Product, Row> rowSelectionBuilder =
+				RowSelectionBuilder.create(Product.class, Row.class);
 
-                // row selection criteria
-                // Caution: The method getSelectedProduct can also return null
-                rowSelectionBuilder.add(Conditions.eq(Row.META.productListElem,
-                                getSelectedProduct("NN10021")));
+		// row selection criteria
+		// Caution: The method getSelectedProduct can also return null
+		rowSelectionBuilder.add(Conditions.eq(Row.META.productListElem,
+				getSelectedProduct("NN10021")));
 
-                // head selection criteria
-                rowSelectionBuilder.addForHead(Conditions.between(Product.META.idno,
-                                "30010", "30015"));
+		// head selection criteria
+		rowSelectionBuilder.addForHead(Conditions.between(Product.META.idno,
+				"30010", "30015"));
 
-                RowQuery<Product, Row> rowQueryProduct =
-                                getDbContext().createQuery(rowSelectionBuilder.build());
+		RowQuery<Product, Row> rowQueryProduct =
+				getDbContext().createQuery(rowSelectionBuilder.build());
 
-                // displays query result
-                if (rowQueryProduct != null) {
-                        getDbContext().out().println("Query-Object: " + rowQueryProduct);
-                        for (Row row : rowQueryProduct) {
-                                getDbContext().out().println(
-                                                row.header().getIdno() + " - " + row.header().getSwd());
-                                getDbContext().out().println(
-                                                "--" + row.getProductListElem().getIdno() + " -- "
-                                                                + row.getProductListElem().getSwd());
-                        }
-                }
-                else {
-                        getDbContext().out().println("Query-Object: null");
-                }
+		// displays query result
+		if (rowQueryProduct != null) {
+			getDbContext().out().println("Query-Object: " + rowQueryProduct);
+			for (Row row : rowQueryProduct) {
+				getDbContext().out().println(
+						row.header().getIdno() + " - " + row.header().getSwd());
+				getDbContext().out().println(
+						"--" + row.getProductListElem().getIdno() + " -- "
+								+ row.getProductListElem().getSwd());
+			}
+		}
+		else {
+			getDbContext().out().println("Query-Object: null");
+		}
 
-        }
+	}
 
-        /**
-         * Selects first product with the specified search word.
-         *
-         * @param swd The search word.
-         * @return Returns the product as instance of SelectableObject.
-         */
-        private SelectableObject getSelectedProduct(String swd) {
-                SelectionBuilder<Product> selectionBuilder =
-                                SelectionBuilder.create(Product.class);
-                selectionBuilder.add(Conditions.eq(Product.META.swd, swd));
-                Product product =
-                                QueryUtil.getFirst(getDbContext(), selectionBuilder.build());
-                return product;
-        }
+	/**
+	 * Selects first product with the specified search word.
+	 *
+	 * @param swd The search word.
+	 * @return Returns the product as instance of SelectableObject.
+	 */
+	private SelectableObject getSelectedProduct(String swd) {
+		SelectionBuilder<Product> selectionBuilder =
+				SelectionBuilder.create(Product.class);
+		selectionBuilder.add(Conditions.eq(Product.META.swd, swd));
+		Product product =
+				QueryUtil.getFirst(getDbContext(), selectionBuilder.build());
+		return product;
+	}
 
 }
